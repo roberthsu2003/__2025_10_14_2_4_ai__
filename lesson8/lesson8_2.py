5a70edf1cc81d8c29954a9f700247af0ee3e3b43
徐國堂 <roberthsu2003@gmail.com>
2025-11-06 11:43:16 +0000

    modify

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -1,5 +1,11 @@
-with open('assets/names.txt', encoding="utf-8") as file:
+import os
+
+# 使用names.txt的絕對路徑
+current_dir = os.path.dirname(os.path.abspath("__file__"))
+file_path = os.path.join(current_dir,'lesson8','assets','names.txt')
+
+with open(file_path, encoding="utf-8") as file:
     content = file.read()
 
 names = content.split(',')
-names
+print(names)
