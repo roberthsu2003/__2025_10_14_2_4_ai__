e50c51261cdf149a9e2ed1be89fde64883bd2522
徐國堂 <roberthsu2003@gmail.com>
2025-11-06 13:22:18 +0000

    modify

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 def get_names(file_name):
     # 使用names.txt的絕對路徑
@@ -8,7 +9,21 @@ def get_names(file_name):
     with open(file_path, encoding="utf-8") as file:
         content = file.read()
 
-    return content.split(',')
+    return content.split('\n')
+
+def get_scores(names, num=10):
+    stu_names = random.sample(names,num)
+    scores = []
+    for name in stu_names:
+        info = {"姓名":name,
+                "國文":random.randint(50, 100),
+                "英文":random.randint(50, 100),
+                "數學":random.randint(50, 100)
+                }
+        scores.append(info)
+    return scores
     
 names = get_names("names.txt")
-print(names)
+num = int(input("請輸入學生數量:"))
+students = get_scores(names,num=num)
+print(students)
